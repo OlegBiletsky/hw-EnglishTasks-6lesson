8af9f762fce241fd8d9461a7f9eede73561b953b
OlegBiletsky <ol.biletsky@gmail.com>
2020-09-19 14:57:01 +0300

    1

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let studentList  = [80, 77 , 68, 88, 95];
+let sum = 0, averageMark, markECTS;
 
-let sum = 0, averageMark;
 for (i = 0; i < studentList.length; i++) {
     sum += studentList[i];
 
@@ -8,4 +8,21 @@ for (i = 0; i < studentList.length; i++) {
 averageMark = sum / studentList.length;
 console.log(averageMark);
 
-// alert(` Середній бал: ${averageMark} . Оцінка за ECTS: ${} . `);
+
+
+if (averageMark < 60 ) {
+    markECTS = "F";
+} else if (averageMark < 70 ) {
+    markECTS = "D";
+} else if (averageMark < 80 ) {
+    markECTS = "C";
+} else if (averageMark < 90 ) {
+    markECTS = "B";
+} else {
+    markECTS = "A";
+}
+
+console.log(markECTS);
+
+alert(` Середній бал: ${averageMark} . Оцінка за ECTS: ${markECTS} . `);
+
